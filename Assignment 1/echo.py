c2ee8e5568e8ae667836210e4097e49677bad03c
dasorensen <david-sorensen-1@uiowa.edu>
2025-02-04 11:58:48 -0600

    Entered method signature for echo.py

---
+++ Assignment 1/echo.py
@@ -0,0 +1,8 @@
+def echo(text: str, repetitions: int = 3) -> str:
+    """Imitate a real-world echo."""
+    pass  # replace this line with your code lines
+
+
+if __name__ == "__main__":
+    text = input("Yell something at a mountain: ")
+    print(echo(text))
