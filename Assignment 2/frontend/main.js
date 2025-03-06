fa5ef427e7a8e82f67d941a1159fc399c7e40808
dasorensen <david-sorensen-1@uiowa.edu>
2025-03-06 11:51:43 -0600

    Completed code for Assignment 2

--- Assignment 2/frontend/main.js
+++ Assignment 2/frontend/main.js
@@ -1,1 +1,32 @@
 const api = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
+
+const displayPopulation = (populations) => {
+    const tbody = document.getElementById('population-rows');
+    tbody.innerHTML = ''
+    const rows = populations.map(x => {
+        return `<tr>
+            <td>${x.Year}</td>
+            <td>${x.Population}</td>
+            </tr>`
+    });
+    tbody.innerHTML = rows.join(' ');
+}
+
+const getPopulations = () => {
+    const xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            data = JSON.parse(xhr.responseText);
+            popData = data["data"]
+            console.log(popData);
+            displayPopulation(popData);
+        }
+    }
+
+    xhr.open('GET', api, true);
+    xhr.send();
+};
+
+(() => {
+    getPopulations();
+})()
