82724d3edc949c10e772664640d39a6268044886
dasorensen <david-sorensen-1@uiowa.edu>
2025-03-06 12:01:56 -0600

    Added comments to main.js

--- Assignment 2/frontend/main.js
+++ Assignment 2/frontend/main.js
@@ -1,5 +1,7 @@
+// Api endpoint for the population data
 const api = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
 
+// Display the population data in a table
 const displayPopulation = (populations) => {
     const tbody = document.getElementById('population-rows');
     tbody.innerHTML = ''
@@ -16,9 +18,14 @@ const getPopulations = () => {
     const xhr = new XMLHttpRequest()
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
+            // use JSON.parse to obtain the information from API
             data = JSON.parse(xhr.responseText);
+
+            // obtain only the "data" portion of the returned object from JSON.parse
             popData = data["data"]
             console.log(popData);
+
+            // call the displayPopulation function to tabulate the population data
             displayPopulation(popData);
         }
     }
@@ -27,6 +34,7 @@ const getPopulations = () => {
     xhr.send();
 };
 
+// IIFE function to parse the population data from the API endpoint upon loading the webpage
 (() => {
     getPopulations();
 })()
