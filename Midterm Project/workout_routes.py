f1779cc6f2627cb6dfde328d900ebf41c7af831a
dasorensen <david-sorensen-1@uiowa.edu>
2025-03-10 14:25:23 -0500

    Finished workout_routes

--- Midterm Project/workout_routes.py
+++ Midterm Project/workout_routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, status
 from workout import Workout, WorkoutRequest
 
 
@@ -22,3 +23,33 @@ async def add_workout(workout: WorkoutRequest) -> Workout:
     )
     workout_list.append(newWorkout)
     return newWorkout
+
+
+@workout_router.get("/{exercise}")
+async def get_workout_by_exercise(
+    exercise: Annotated[str, "Name of exercise"],
+) -> Workout:
+    for workout in workout_list:
+        if workout.exercise == exercise:
+            return workout
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"The item with exercise={exercise} is not found",
+    )
+
+
+@workout_router.delete("/{exercise}")
+async def delete_workout_by_exercise(
+    exercise: Annotated[str, "Name of exercise"],
+) -> dict:
+    for i in range(len(workout_list)):
+        workout = workout_list[i]
+        if workout.exercise == exercise:
+            workout_list.pop(i)
+            return {"message": f"The item with exercise={exercise} is removed."}
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"The item with exercise={exercise} is not found",
+    )
