c6bfe714435b65ea03328844fe7b59aff6af314a
dasorensen <david-sorensen-1@uiowa.edu>
2025-03-10 23:19:40 -0500

    Added favicon and basic functionality is working

--- Midterm Project/frontend/main.js
+++ Midterm Project/frontend/main.js
@@ -1,10 +1,48 @@
 const api = 'http://127.0.0.1:8000/workouts';
 
+document.getElementById('save-new-workout').addEventListener('click', (e) => {
+    e.preventDefault();
+    postWorkout();
+    const closeBtn = document.getElementById('add-close');
+    closeBtn.click();
+});
+
+const postWorkout = () => {
+    const exerciseInput = document.getElementById('new-exercise');
+    const exercise = exerciseInput.value;
+    const setsInput = document.getElementById('new-sets');
+    const sets = setsInput.value;
+    const repsInput = document.getElementById('new-reps');
+    const reps = repsInput.value;
+    const notesInput = document.getElementById('new-notes');
+    const notes = notesInput.value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 201) {
+            getWorkouts();
+        }
+    };
+
+    xhr.open('POST', api, true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send(JSON.stringify({exercise, sets, reps, notes}));
+};
 
 
 
+const deleteWorkout = (exercise) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            data = data.filter((x) => x.exercise != exercise);
+            getWorkouts();
+        }
+    };
 
-
+    xhr.open('DELETE', `${api}/${exercise}`, true);
+    xhr.send();
+};
 
 const displayWorkouts = (workouts) => {
     const tbody = document.getElementById('workout-rows');
@@ -16,10 +54,7 @@ const displayWorkouts = (workouts) => {
             <td>${x.reps}</td>
             <td>${x.notes}</td>
             <td>
-            <button type="button" class="btn btn-primary">Edit</button>
-            </td>
-            <td>
-            <button type="button" class="btn btn-danger">Delete</button>
+            <button onClick="deleteWorkout('${x.exercise}')" type="button" class="btn btn-danger">Delete</button>
             </td>
         </tr>`
     });
